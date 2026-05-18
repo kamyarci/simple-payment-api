33e05da613a35d40835b4b4bd1b7d87d98deeb90
Kamyla Arci <kamylaarci@gmail.com>
2026-05-18 15:26:25 -0300

    feat: initial config

---
+++ src/test/java/com/kamyla/simple_payment_api/SimplePaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamyla.simple_payment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
