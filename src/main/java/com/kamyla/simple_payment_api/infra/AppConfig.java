f98c6385263ed7bd20900c09d097ead6adc94854
Kamyla Arci <kamylaarci@gmail.com>
2026-05-18 17:59:03 -0300

    feat: create AppConfig

---
+++ src/main/java/com/kamyla/simple_payment_api/infra/AppConfig.java
@@ -0,0 +1,15 @@
+package com.kamyla.simple_payment_api.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
