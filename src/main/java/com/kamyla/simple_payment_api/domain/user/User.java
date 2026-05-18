55266daa76f9b792c888cda9ba07641fbb60afc7
Kamyla Arci <kamylaarci@gmail.com>
2026-05-18 15:30:13 -0300

    feat: create User entity

---
+++ src/main/java/com/kamyla/simple_payment_api/domain/user/User.java
@@ -0,0 +1,41 @@
+package com.kamyla.simple_payment_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balanche; //saldo do usuario
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+}
---
+++ src/main/java/com/kamyla/simple_payment_api/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.kamyla.simple_payment_api.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
