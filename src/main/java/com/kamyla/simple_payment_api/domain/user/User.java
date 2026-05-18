cb099559efef1425c3d9762ddf6bc0189b672b01
Kamyla Arci <kamylaarci@gmail.com>
2026-05-18 15:33:32 -0300

    feat: add NoArgsConstructor to User entity

--- src/main/java/com/kamyla/simple_payment_api/domain/user/User.java
+++ src/main/java/com/kamyla/simple_payment_api/domain/user/User.java
@@ -1,10 +1,7 @@
 package com.kamyla.simple_payment_api.domain.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User {
 
