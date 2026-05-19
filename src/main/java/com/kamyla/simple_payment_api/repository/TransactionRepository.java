3220d3e1aa67d66ab5f10928057dd1e4301104c4
Kamyla Arci <kamylaarci@gmail.com>
2026-05-19 15:44:34 -0300

    refactor: rename repository packages

--- src/main/java/com/kamyla/simple_payment_api/repository/TransactionRepository.java
+++ src/main/java/com/kamyla/simple_payment_api/repository/TransactionRepository.java
@@ -1,4 +1,4 @@
-package com.kamyla.simple_payment_api.repositories;
+package com.kamyla.simple_payment_api.repository;
 
 import com.kamyla.simple_payment_api.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
