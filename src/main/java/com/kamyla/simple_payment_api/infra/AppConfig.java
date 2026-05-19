3cb67f462f3895fedead8e81ba8a83c5f1562e3a
Kamyla Arci <kamylaarci@gmail.com>
2026-05-19 15:34:53 -0300

    feat: update AppConfig

--- src/main/java/com/kamyla/simple_payment_api/infra/AppConfig.java
+++ src/main/java/com/kamyla/simple_payment_api/infra/AppConfig.java
@@ -2,14 +2,16 @@ package com.kamyla.simple_payment_api.infra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestClient;
 
 @Configuration
+@EnableAsync
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestClient restClient() {
+        return RestClient.create();
     }
 
 }
